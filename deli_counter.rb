c1a32ba8034c926503cb14a286471cee4a22cc99
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-18 02:21:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,10 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
+  katz_deli.each.with_index(1) do |name, index|
     puts  "Welcome, #{name}. You are number #{index} in line."
     end
-
+end
 
 def now_serving(deli)
   if deli.size <= 0
