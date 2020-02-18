a03c28ebf0395076d7c5690edaab862411a3ae7a
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-18 02:48:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-    puts  "Welcome, #{name}. You are number #{katz_deli[-1]} in line."
+    puts  "Welcome, #{name}. You are number #{} in line."
 end
 
 def now_serving(deli)
